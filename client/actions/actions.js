e2c044f23e62b7ccf7bfc79064a3efb4f3d0cb59
Yankun Song <yankun.l.song@gmail.com>
2022-05-02 19:03:52 -0500

    finished set up
    Co-authored-by: Erica ericaysoh@gmail.com
    
    Co-authored-by: David https://github.com/koyykdy
    
    Co-authored-by: Trey quantumthink128@gmail.com
    
    Co-authored-by: Yankun Song yankun.L.song@gmail.com

---
+++ client/actions/actions.js
@@ -0,0 +1,17 @@
+/**
+ * ************************************
+ *
+ * @module  actions.js
+ * @author
+ * @date
+ * @description Action Creators
+ *
+ * ************************************
+ */
+
+// import actionType constants
+import * as types from '../constants/actionTypes';
+
+export const sampleActionCreator = () => ({
+    type: types.SAMPLE_ACTION,
+});
